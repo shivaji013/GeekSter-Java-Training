ccc37ebad8ded39d1611d7e3c53429fc0ce17f2f
shivaji <shivajixxx13@gmail.com>
2022-01-26 13:49:57 +0530

    new pro1to100.java

---
+++ pro1to100.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class pro1to100
+{
+    public static void main(String[] ar)
+    {
+        int i;
+        for(i=1;i<=1000;i++)
+        {
+            System.out.print(i+"     ");
+        }
+    }
+}
