2a8fb85431427a484dea3639a6fffb5cf030a12b
shivaji013 <97774542+shivaji013@users.noreply.github.com>
2022-02-18 01:31:20 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,20 @@
+
+public class sum {
+ public static void main(String args[])
+ {
+	 int num1=20;
+	 int num2=30;
+
+	 int result=num1+num2;
+	 System.out.println("sum  "+result);
+	 
+	  result=num1-num2;
+	 System.out.println("Sub  "+result);
+	 
+	 result=num1*num2;
+	 System.out.println("Mul  "+result);
+	 
+	 result=num1/num2;
+	 System.out.println("Devided  "+result);
+ }
+}
