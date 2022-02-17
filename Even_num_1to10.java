2fbded002a7b13a7eb4772d875184f13f0a3c831
shivaji013 <97774542+shivaji013@users.noreply.github.com>
2022-02-18 02:16:16 +0530

    WAP even number using for loop & while loop

---
+++ Even_num_1to10.java
@@ -0,0 +1,23 @@
+
+public class Even_num_1to10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		 * for(int i=1;i<=10;i++) { if(i%2==0) { System.out.println("Even num ::"+i); } }
+		 */	
+		
+		int i;
+		i=1;
+		while(i<=10)
+		{
+			if(i%2==0)
+			{
+			System.out.println("Even number ::  "+i);
+			}
+			i++;
+		}
+	}
+}
