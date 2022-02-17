61573d87a2b02a6dabe3862124fec48189222ae2
shivaji013 <97774542+shivaji013@users.noreply.github.com>
2022-02-18 01:52:54 +0530

    Wap to print 10 to 1 using doWhile loop

---
+++ whileloop10to1.java
@@ -0,0 +1,15 @@
+
+public class whileloop10to1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i;
+		  i=10;
+		do {
+			System.out.println(i);
+			i--;
+		}while(i>=1);
+	}
+
+}
